b90d4f0282d951a1e4f12f6da0c1f3e2a26aa516
Markus Wenzel <mwenzel@owncloud.com>
2020-10-28 12:24:26 +0100

    added some more functionality and polished README

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ This simple implementation should help debugging and testing of applications
 relying on working with an APM. It should help implementing the right behavior
 working with the `MRHSession` cookie and working with the ambigous `302`
 redirect.
+
+Currently works only with `http` but can be extended to `https`.
+
+## How to run
+You can set up following vars:
+`SIMPLE_APM_PORT`: Defaults to `8080` but can be set up to any port.
+`SIMPLE_APM_PROXIED_SERVICE`: Defaults to `http://localhost:8081`.
+`SIMPLE_APM_COOKIE_TTL`: Defaults to `360` seconds (5 minutes).
+
+Example:
+```
+SIMPLE_APM_PROXIED_SERVICE=http://localhost:8082  SIMPLE_APM_COOKIE_TTL=3600  SIMPLE_APM_PORT=8081 ./simple-apm-start-allow-service
+```
--- main.go
+++ main.go
@@ -8,16 +8,41 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+	"strconv"
 	"time"
 )
 
+// Service port
+var port = "8080"
+
+// Time to live for the MRHSession cookie
+var cookieTTL = "360"
+
+// Proxied service
+var proxiedService = "http://localhost:8081"
+
+/*
+	Request map to redirect to the right ressource after issuing the MRHSession
+	cookie.
+*/
 var requests = make(map[string]string)
 
+// Issued cookies together with their expiration time
 var cookies = make(map[string]time.Time)
 
+/*
+	Redirect with issuing the cookie
+	It also stores the originally requested ressource in the "requests" map
+	and the expiration time of the issued cookie in the "cookies" map.
+*/
 func issueCookie(response http.ResponseWriter, request *http.Request) {
-	ttl := 5 * time.Minute
-	expire := time.Now().Add(ttl)
+	ttl, err := strconv.Atoi(cookieTTL)
+	if err != nil {
+		fmt.Printf("Something went terribly wrong...")
+		log.Fatal(err)
+	}
+	expire := time.Now().Add(time.Duration(ttl) * time.Second)
 	val, _ := randomHex(32)
 	cookie := http.Cookie{
 		Name:    "MRHSession",
@@ -25,50 +50,55 @@ func issueCookie(response http.ResponseWriter, request *http.Request) {
 		Expires: expire,
 	}
 	http.SetCookie(response, &cookie)
-	requests[cookie.Value] = request.URL.Path
-	cookies[cookie.Value] = expire
+	requests[cookie.Value] = request.URL.Path // Store requested ressource
+	cookies[cookie.Value] = expire            // Store expiration of MRHSession cookie
 	http.Redirect(response, request, "/my.policy", http.StatusFound)
 }
 
+// Proxy handler for imitating the APM flow
 func proxyHandler(response http.ResponseWriter, request *http.Request) {
 	cookie, err := request.Cookie("MRHSession")
 	if err == http.ErrNoCookie {
-		issueCookie(response, request)
+		issueCookie(response, request) // No cookie -> Issue new one
 	} else if err != nil {
-		fmt.Println(err)
+		fmt.Println(err) // hopefully never happens...
 		fmt.Fprintf(response, "Something went terribly wrong... %s", err)
 	} else {
 		sub := cookies[cookie.Value].Sub(time.Now())
 		fmt.Println(sub)
 		if sub <= 0 {
+			// If cookie is expired -> Issue new one
 			issueCookie(response, request)
 		} else {
-			serveProxy("http://localhost:8081", response, request)
+			// Proxy the request
+			url, _ := url.Parse(proxiedService)
+			proxy := httputil.NewSingleHostReverseProxy(url)
+			proxy.ServeHTTP(response, request)
 		}
 	}
 }
 
+// Handler for imitating 302 responses of the APM
 func myPolicyHandler(response http.ResponseWriter, request *http.Request) {
 	cookie, err := request.Cookie("MRHSession")
 	if request.Method != http.MethodGet {
+		// If the request method is not a GET request -> Evil!
 		http.Redirect(response, request, "/vdesk/hangup.php3", http.StatusFound)
 	} else if err == http.ErrNoCookie {
+		// This should never happen, but if so -> Start from the beginning
 		http.Redirect(response, request, "/", http.StatusFound)
 	} else {
+		// Whoop! This is the lucky way! Let's start with the cookie.
 		http.Redirect(response, request, requests[cookie.Value], http.StatusFound)
 	}
 }
 
+// Handler for evil hangup page
 func hangupHandler(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(response, "Evil page...")
 }
 
-func serveProxy(target string, response http.ResponseWriter, request *http.Request) {
-	url, _ := url.Parse(target)
-	proxy := httputil.NewSingleHostReverseProxy(url)
-	proxy.ServeHTTP(response, request)
-}
-
+// Utility function for creating something similar to a real MRHSession uuid
 func randomHex(n int) (string, error) {
 	bytes := make([]byte, n)
 	if _, err := rand.Read(bytes); err != nil {
@@ -78,8 +108,27 @@ func randomHex(n int) (string, error) {
 }
 
 func main() {
+	envPort := os.Getenv("SIMPLE_APM_PORT")
+	if envPort != "" {
+		port = envPort
+	}
+
+	envProxiedService := os.Getenv("SIMPLE_APM_PROXIED_SERVICE")
+	if envProxiedService != "" {
+		proxiedService = envProxiedService
+	}
+
+	envCookieTTL := os.Getenv("SIMPLE_APM_COOKIE_TTL")
+	if envCookieTTL != "" {
+		cookieTTL = envCookieTTL
+	}
+
+	fmt.Printf("Listening on: http://localhost:%s\n", port)
+	fmt.Printf("Proxied service: %s\n", proxiedService)
+	fmt.Printf("Cookie ttl set to: %s seconds\n", cookieTTL)
+
 	http.HandleFunc("/", proxyHandler)
 	http.HandleFunc("/my.policy", myPolicyHandler)
 	http.HandleFunc("/vdesk/hangup.php3", hangupHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
